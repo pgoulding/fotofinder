28c27c3aa5edbf44ca26690f35196bec2beece9c
Patrick Goulding <patrickgoulding@gmail.com>
2019-02-22 13:40:14 -0700

    added query selectors for the form buttons and search

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 /*---------- Query Selectors -----------*/
-
+var searchBar = document.querySelector('#search-input');
+var inputTitle = document.querySelector('#title-input');
+var inputCaption = document.querySelector('#caption-input');
+var buttonChooseFile = document.querySelector('#choose-file');
+var buttonViewFavorites = document.querySelector('#view-favorites');
+var buttonUpload = document.querySelector('#add-to-album');
 
 /*---------- Global Variables ----------*/
 
